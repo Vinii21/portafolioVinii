381965b003ec91aff43ad79c326f294875b10656
unknown <vichavarria21@gmail.com>
2022-10-28 06:54:28 -0600

    Implementacion de Base de datos

---
+++ index.js
@@ -0,0 +1,34 @@
+import { consulta } from './firebase.js';
+const contenedorProyectos = document.getElementById('contenedor-proyectos');
+
+window.addEventListener('DOMContentLoaded', async () => {
+    const querySnapshop = await consulta()
+
+    let html = ''
+
+    querySnapshop.forEach(doc => {
+        const proyecto = doc.data();
+        html +=`
+            <div class="projectCard" style="background-image: url('${proyecto.background}');">
+                <div class="projectCard--info">
+                    <h3>${proyecto.title}</h3>
+                    <ul>
+                        ${proyecto.skills.map(item=>{
+                            return(`<li>${item}</li>`)
+                        })}
+                    </ul>
+                    <div class="iconLinks">
+                        <a href=${proyecto.link_project}><i class="bi bi-arrow-up-right-square-fill"></i></a>
+                        <a href=${proyecto.link_code}><i class="bi bi-github"></i></a>
+                    </div>
+                </div>
+            </div>
+        `
+    })
+
+    contenedorProyectos.innerHTML = html
+    
+})
+
+
+
--- js/sections.js
+++ js/sections.js
@@ -1,39 +1,39 @@
 //Condicional para que la animacion se vea en desktop y no en tablet o mobile
 if(screen.width >= 908) {
-    const cards = document.querySelectorAll('.container__projects .projectCard');
-    const cardsInfo = document.querySelectorAll('.container__projects .projectCard--info');
-    
-    //Con el loop for recorremos el arreglo donde estan las cajas que tienen la info de cada proyecto
-    //Se le da un evento para que haga algo cuando el muose este sobre la caja
-    for(let i = 0; i < cards.length; i++){
-        cards[i].addEventListener('mouseover', ()=>{
-            stateCard = 5;
-            mostrar(i);
-        });
-    };
-    
-    /* La funcion que muestra la caja oculta de info */
-    function mostrar(index){
-        for(let i = 0; i < cardsInfo.length; i++){
-            if(i === index){
-                cardsInfo[i].style.top = "0px";
-                cardsInfo[i].addEventListener('mouseout', () => {
-                    ocultar(i);
-                });
+    setTimeout(()=>{
+        const cards = document.querySelectorAll('.container__projects .projectCard');
+        const cardsInfo = document.querySelectorAll('.container__projects .projectCard--info');
+        
+        //Con el loop for recorremos el arreglo donde estan las cajas que tienen la info de cada proyecto
+        //Se le da un evento para que haga algo cuando el muose este sobre la caja
+        for(let i = 0; i < cards.length; i++){
+            cards[i].addEventListener('mouseover', ()=>{
+                mostrar(i);
+            });
+        };
+        
+        /* La funcion que muestra la caja oculta de info */
+        function mostrar(index){
+            for(let i = 0; i < cardsInfo.length; i++){
+                if(i === index){
+                    cardsInfo[i].style.top = "0px";
+                    cardsInfo[i].addEventListener('mouseout', () => {
+                        ocultar(i);
+                    });
+                };
             };
         };
-    };
-    
-    /* la funcion que oculta la caja cuando no se esta sobre ella */
-    function ocultar(index){
-        for(let i = 0; i < cardsInfo.length; i++){
-            if(i === index){
-                cardsInfo[i].style.top = "200px";
+        
+        /* la funcion que oculta la caja cuando no se esta sobre ella */
+        function ocultar(index){
+            for(let i = 0; i < cardsInfo.length; i++){
+                if(i === index){
+                    cardsInfo[i].style.top = "200px";
+                };
             };
         };
-    };
-} else {
-    console.log('Todo en order')
+    }, 1000);
 }
+
 
 

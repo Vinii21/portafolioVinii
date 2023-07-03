82a394a8390668f16cd7472bb6f8a25f18d8f45d
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-07-03 10:04:59 -0600

    Actualización en desarrollo, carrusel de proyectos en desarrollo

---
+++ assets/js/swipperInit.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  
